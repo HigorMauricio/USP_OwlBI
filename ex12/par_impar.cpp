857005c7b4d57a973c0873083fe12f01cbde5e4d
HigorMauricio <higormauriciosilva@gmail.com>
2025-05-10 21:23:03 -0300

    commit ex12

---
+++ ex12/par_impar.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "resolucao do exercicio https://neps.academy/br/exercise/148\n";
+    int B, C;
+    cin >> B;
+    cin >> C;
+    if ((B+C) % 2 == 0) cout << "Bino";
+    else cout << "Cino";
+    return 0;
+}
